a353f3d7c53a6933a33683b6d9fbbab27ac73c61
EunjunEddieLee <alohaeddielee@gmail.com>
2025-01-26 15:55:15 +0900

    draw a triangle, a rectangle

--- opengl_example/src/context.cpp
+++ opengl_example/src/context.cpp
@@ -8,6 +8,63 @@ ContextUPtr Context::Create() {
 }
 
 bool Context::Init() {
+  
+  float vertices[] = {
+    // first triangle
+    0.5f, 0.5f, 0.0f, // top right
+    0.5f, -0.5f, 0.0f, // bottom rght
+    -0.5f, 0.5f, 0.0f, // top left
+    //second triangle
+    0.5f, -0.5f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, // bottom left
+    -0.5f, 0.5f, 0.0f, // top left
+  };
+
+  /*
+  ** Vertex Buffer Object (VBO)
+
+  glGenBuffers(): 새로운 buffer object를 만든다
+  glBindBuffer(): 지금부터 사용할 buffer object 지정
+    - GL_ARRAY_BUFFER: 사용할 buffer object가 vertex data를 저장할 용도임을 알려줌
+  glBufferData(): 지정된 buffer에 데이터를 복사
+    - 데이터의 총 크기, 데이터 포인터, 용도를 지정
+    - GL_STATIC|DINAMIC|STREAM_DRAW|COPY|READ : 용도를 위한 flag
+
+    - 정점 데이터 구조 :
+    | vertex 1  | vertex 2  | vertex 3  |
+    | x | y | z | x | y | z | x | y | z |
+    0   4   8   12          24          36
+
+  위의 정점 데이터의 구조를 알려주는 오브젝트가 필요함
+  (각 정점이 몇 byte? 두 정점이 몇 byte만큼 떨어짐? 등)
+  --> VAO
+  --------------------------------------------------
+  ** Vertex Array Object (VAO)
+
+  glGenVertexArrays(): VAO 생성
+  glBindVertexArray(): 지금부터 사용할 VAO 설정
+  glEnableVertexAttribArray(n): 정점 attribute 중 n번째를 사용하도록 설정
+  glVertexAttribPointer(n, size, type, normalize, stride, offset):
+    - n: 정점의 n번째 attribute
+    - size: 해당 attribute가 몇 개의 값으로 구성되어 있는가?
+    - type: 해당 attribute의 데이터 타입
+    - normalized: 0~1 사이의 값인가
+    - stride: 두 정점 간의 간격 (byte 단위)
+    - offset: 첫 정점의 해당 attribute 까지의 간격 (byte 단위)
+
+  순서 주의 - VAO binding -> VBO binding -> vertex attribute setting
+  */
+  glGenVertexArrays(1, &m_vertexArrayObject);
+  glBindVertexArray(m_vertexArrayObject);
+  //vao 생성 후에 buffer를 만들어야 연결 가능
+  glGenBuffers(1, &m_vertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, vertices, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+
   // OpenGL 함수 로딩 후에야 shader 불러오기 위한 함수들 사용 가능
   ShaderPtr vertShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
   ShaderPtr fragShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
@@ -24,17 +81,19 @@ bool Context::Init() {
   // 화면 클리어
   glClearColor(0.0f, 0.1f, 0.2f, 0.3f); // 컬러 프레임버퍼 화면을 클리어 할 색상 지정
 
-  // shader progam test
-  uint32_t vao = 0;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
   return true;
 }
 
+/*
+glDrawArray(primitive, offset, count)
+  - 현재 설정된 program, VBO, VAO로 그림을 그린다
+  - primitive: 그리고자 하는 primitive 타입
+  - offset: 첫 정점의 index
+  - count: 몇 개의 정점이 있는가
+*/
 void Context::Render() {
   glClear(GL_COLOR_BUFFER_BIT); // 프레임 색상 버퍼 클리어
 
-  glUseProgram(m_program->Get());
-  glDrawArrays(GL_POINTS, 0, 1);
+  glUseProgram(m_program->Get()); 
+  glDrawArrays(GL_TRIANGLES, 0, 6);
 }
