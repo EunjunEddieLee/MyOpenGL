8fdda08f280f9de787f0e5c99e304ed6e00c4b05
EunjunEddieLee <alohaeddielee@gmail.com>
2025-02-02 21:49:06 +0900

    Day 6 end. ModelViewTransform matrix, rotating cubes

--- opengl_example/src/context.cpp
+++ opengl_example/src/context.cpp
@@ -9,16 +9,45 @@ ContextUPtr Context::Create() {
 }
 
 bool Context::Init() {
-  // [x, y, z, r, g, b, s, t] 
   float vertices[] = {
-    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+    0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+    0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+
+    -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+    0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+    0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+    -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
   };
+
   uint32_t indices[] = {
-    0, 1, 3, // first triangle
-    1, 2, 3, // second triangle
+    0,  2,  1,  2,  0,  3,
+    4,  5,  6,  6,  7,  4,
+    8,  9, 10, 10, 11,  8,
+    12, 14, 13, 14, 12, 15,
+    16, 17, 18, 18, 19, 16,
+    20, 22, 21, 22, 20, 23,
   };
 
   /*
@@ -79,13 +108,12 @@ bool Context::Init() {
 
   m_vertexLayout = VertexLayout::Create();
   //vao 생성 후에 buffer를 만들어야 연결 가능
-  m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 32);
+  m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 120);
 
-  m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
-  m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 3);
-  m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, sizeof(float) * 6);
+  m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+  m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, sizeof(float) * 3);
 
-  m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
+  m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 36);
 
   // OpenGL 함수 로딩 후에야 shader 불러오기 위한 함수들 사용 가능
   ShaderPtr vertShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
@@ -111,7 +139,7 @@ bool Context::Init() {
     image->GetWidth(), image->GetHeight(), image->GetChannelCount());
   m_texture = Texture::CreateFromImage(image.get());
 
-  auto image2 = Image::Load("./image/awesomeface.png");
+  auto image2 = Image::Load("./image/chillguy.png");
   if (!image2)
     return false;
   SPDLOG_INFO("image2: {}x{}, {} channels",
@@ -125,17 +153,8 @@ bool Context::Init() {
   glBindTexture(GL_TEXTURE_2D, m_texture2->Get());
 
   m_program->Use();
-  glUniform1i(glGetUniformLocation(m_program->Get(), "tex"), 0);
-  glUniform1i(glGetUniformLocation(m_program->Get(), "tex2"), 1);
-
-
-  // 0.5배 축소후 z축으로 90도 회전하는 행렬
-  auto transform = glm::rotate(
-    glm::scale(glm::mat4(1.0f), glm::vec3(0.5f)),
-    glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)
-    );
-  auto transformLoc = glGetUniformLocation(m_program->Get(), "transform");
-  glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+  m_program->SetUniform("tex", 0);
+  m_program->SetUniform("tex2", 1);
 
   return true;
 }
@@ -157,18 +176,35 @@ glDrawElements(primitive, count, type, pointer/offset)
   - pointer/offset: 그리고자 하는 EBO의 첫 데이터로부터의 오프셋
 */
 void Context::Render() {
-  glClear(GL_COLOR_BUFFER_BIT); // 프레임 색상 버퍼 클리어
+  std::vector<glm::vec3> cubePositions = {
+    glm::vec3( 0.0f, 0.0f, 0.0f),
+    glm::vec3( 2.0f, 5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f, 3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f, 2.0f, -2.5f),
+    glm::vec3( 1.5f, 0.2f, -1.5f),
+    glm::vec3(-1.3f, 1.0f, -1.5f),
+  };
 
-  // // First Animation code
-  // static float time = 0.0f;
-  // float t = sinf(time) * 0.5f + 0.5f;
-  // auto loc = glGetUniformLocation(m_program->Get(), "color");
-  // m_program->Use();
-  // glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t)*(1.0f-t), 1.0f);
-  // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
 
-  // time += 0.016f;
+  auto projection = glm::perspective(glm::radians(45.0f),
+    (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.01f, 20.0f);
+  auto view = glm::translate(glm::mat4(1.0f),
+    glm::vec3(0.0f, 0.0f, -3.0f));
 
-  m_program->Use();
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  for (size_t i = 0; i < cubePositions.size(); i++){
+    auto& pos = cubePositions[i];
+    auto model = glm::translate(glm::mat4(1.0f), pos);
+    model = glm::rotate(model,
+      glm::radians((float)glfwGetTime() * 120.0f + 20.0f * (float)i),
+      glm::vec3(1.0f, 0.5f, 0.0f));
+    auto transform = projection * view * model;
+    m_program->SetUniform("transform", transform);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+  }
 }
